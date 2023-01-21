d9446e681e37b627f60b57f9349e59ea7d9c2490
Mathis Weber <mathis.c.weber@web.de>
2023-01-21 16:19:17 +0100

    Initial commit

---
+++ lib/keepass/db.go
@@ -0,0 +1,17 @@
+package keepass
+
+import (
+    "fmt"
+    "time"
+    "math/rand"
+    "errors"
+)
+
+func LoadDB(path string) (string, error) {
+    time.Sleep(500 * time.Millisecond)
+    rand.Seed(time.Now().UnixNano())
+    if (rand.Int() % 2) == 1 {
+        return "", errors.New(fmt.Sprintf("Failed to load DB at %s", path))
+    }
+    return fmt.Sprintf("Content of %s:\nnuser=johndoe\npassword=foobar", path), nil
+}
