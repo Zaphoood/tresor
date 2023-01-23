52ba83f7a13f2cee566a82199ad7046a8bb88da7
Mathis Weber <mathis.c.weber@web.de>
2023-01-23 20:49:24 +0100

    refactor: rename `ui` -> `tui`

--- cmd/tresor/main.go
+++ cmd/tresor/main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"os"
 	tea "github.com/charmbracelet/bubbletea"
-    "github.com/Zaphoood/tresor/pkg/ui"
+    "github.com/Zaphoood/tresor/pkg/tui"
 )
 
 
 func main() {
-    p := tea.NewProgram(ui.NewModel())
+    p := tea.NewProgram(tui.NewModel())
     if _, err := p.Run(); err != nil {
         fmt.Printf("Error: %s", err)
         os.Exit(1)
