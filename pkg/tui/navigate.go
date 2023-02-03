6a886cbc9a1fd1b2a6ca52a39319362bbb589078
Mathis Weber <mathis.c.weber@web.de>
2023-02-03 19:46:40 +0100

    refactor: restructure table loading

--- pkg/tui/navigate.go
+++ pkg/tui/navigate.go
@@ -4,27 +4,27 @@ import (
 	"fmt"
 	"log"
 
-	kp "github.com/Zaphoood/tresor/lib/keepass"
+	"github.com/Zaphoood/tresor/lib/keepass"
 	"github.com/Zaphoood/tresor/lib/keepass/parser"
-	"github.com/charmbracelet/bubbles/table"
+
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/bubbles/table"
 	"github.com/charmbracelet/lipgloss"
 )
 
 /* Model for navigating the Database in order to view and edit entries */
 
 type Navigate struct {
-	parent       table.Model
-	selector     table.Model
-	preview      table.Model
-	groupsCenter int
-	path         []int
-	err          error
+	parent   table.Model
+	selector table.Model
+	preview  table.Model
+	path     []int
+	err      error
 
 	windowWidth  int
 	windowHeight int
 
-	database *kp.Database
+	database *keepass.Database
 }
 
 var columns []table.Column = []table.Column{
@@ -37,7 +37,7 @@ var columnsWide []table.Column = []table.Column{
 	{Title: "Entries", Width: 10},
 }
 
-func NewNavigate(database *kp.Database, windowWidth, windowHeight int) Navigate {
+func NewNavigate(database *keepass.Database, windowWidth, windowHeight int) Navigate {
 	n := Navigate{
 		path:         []int{0},
 		windowWidth:  windowWidth,
@@ -58,80 +58,76 @@ func NewNavigate(database *kp.Database, windowWidth, windowHeight int) Navigate 
 		table.WithWidth(windowWidth-n.parent.Width()-n.selector.Width()),
 		table.WithColumns(columnsWide),
 	)
-	n.updateAllTables()
+	n.updateAll()
 
 	return n
 }
 
-func (m *Navigate) updateAllTables() {
-	if len(m.path) == 0 {
-		m.parent.SetRows([]table.Row{})
+func (n *Navigate) updateAll() {
+	if len(n.path) == 0 {
+		n.parent.SetRows([]table.Row{})
 	} else {
-		_, _, err := m.updateTable(&m.parent, m.path[:len(m.path)-1])
-		if err != nil {
-			log.Printf("ERROR: %s", err)
-		}
-		m.parent.SetCursor(m.path[len(m.path)-1])
+		updateTable(&n.parent, n.database.Parsed(), n.path[:len(n.path)-1])
+		n.parent.SetCursor(n.path[len(n.path)-1])
 	}
-	var err error
-	m.groupsCenter, _, err = m.updateTable(&m.selector, m.path)
-	if err != nil {
-		log.Printf("ERROR: %s", err)
-	}
-	m.selector.SetCursor(0)
+	updateTable(&n.selector, n.database.Parsed(), n.path)
+	n.selector.SetCursor(0)
 
-	m.updateRightTable()
+	n.updatePreview()
 }
 
-func (m *Navigate) updateRightTable() {
-	cursor := m.selector.Cursor()
+func (n *Navigate) updatePreview() {
+	cursor := n.selector.Cursor()
 	// If a table is empty and the 'down' or 'up' key is pressed, the cursor becomes -1
 	// This may be a bug in Bubbles? Might also be intended
 	if cursor < 0 {
-		m.preview.SetRows([]table.Row{})
+		n.preview.SetRows([]table.Row{})
 		return
 	}
-	item, err := m.database.Parsed().GetItem(append(m.path, cursor))
+	item, err := n.database.Parsed().GetItem(append(n.path, cursor))
 	if err != nil {
-		log.Printf("ERROR: %s", err)
+		n.preview.SetRows([]table.Row{
+			{err.Error(), ""},
+		})
 		return
 	}
 	switch item := item.(type) {
 	case parser.Group:
 		// A group is focused
-		m.setItems(&m.preview, item.Groups, item.Entries)
+		setItems(&n.preview, item.Groups, item.Entries)
 	case parser.Entry:
 		// An entry is focused
-		m.loadEntry(&m.preview, item)
+		n.loadEntry(&n.preview, item)
 	default:
-		log.Printf("ERROR: Expected Group or Entry")
+		log.Printf("ERROR: Expected Group or Entry in updatePreview")
 	}
 }
 
-func (m *Navigate) updateTable(t *table.Model, path []int) (int, int, error) {
-	groups, entries, err := m.database.Parsed().ListPath(path)
+func updateTable(t *table.Model, d *parser.Document, path []int) {
+	groups, entries, err := d.ListPath(path)
 	if err != nil {
-		return 0, 0, err
+		t.SetRows([]table.Row{
+			{err.Error(), ""},
+		})
 	}
 	if len(groups)+len(entries) > 0 {
-		m.setItems(t, groups, entries)
+		setItems(t, groups, entries)
 	} else {
 		t.SetRows([]table.Row{
 			{"(No entries)", ""},
 		})
 	}
-	return len(groups), len(entries), nil
 }
 
-func (m *Navigate) loadEntry(t *table.Model, entry parser.Entry) {
+func (n *Navigate) loadEntry(t *table.Model, entry parser.Entry) {
 	title := entry.TryGet("Title", "(No title)")
 	rows := []table.Row{
 		{fmt.Sprintf("Title: %s", title), ""},
 	}
-	m.preview.SetRows(rows)
+	n.preview.SetRows(rows)
 }
 
-func (m *Navigate) setItems(t *table.Model, groups []parser.Group, entries []parser.Entry) {
+func setItems(t *table.Model, groups []parser.Group, entries []parser.Entry) {
 	rows := make([]table.Row, len(groups)+len(entries))
 	for i, group := range groups {
 		rows[i] = table.Row{group.Name, fmt.Sprint(len(group.Groups) + len(group.Entries))}
@@ -143,53 +139,57 @@ func (m *Navigate) setItems(t *table.Model, groups []parser.Group, entries []par
 	t.SetRows(rows)
 }
 
-func (m *Navigate) moveLeft() {
-	if len(m.path) == 0 {
+func (n *Navigate) moveLeft() {
+	if len(n.path) == 0 {
 		return
 	}
-	m.path = m.path[:len(m.path)-1]
-	m.updateAllTables()
+	n.path = n.path[:len(n.path)-1]
+	n.updateAll()
 }
 
-func (m *Navigate) moveRight() {
-	cursor := m.selector.Cursor()
-	if cursor < 0 || cursor >= m.groupsCenter {
+func (n *Navigate) moveRight() {
+	cursor := n.selector.Cursor()
+	selected, err := n.database.Parsed().GetItem(append(n.path, cursor))
+	if err != nil {
 		return
 	}
-	m.path = append(m.path, m.selector.Cursor())
-	m.updateAllTables()
+	if _, ok := selected.(parser.Group); !ok {
+		return
+	}
+	n.path = append(n.path, n.selector.Cursor())
+	n.updateAll()
 }
 
-func (m Navigate) Init() tea.Cmd {
+func (n Navigate) Init() tea.Cmd {
 	return nil
 }
 
-func (m Navigate) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (n Navigate) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.windowWidth = msg.Width
-		m.windowHeight = msg.Height
-		return m, globalResizeCmd(msg.Width, msg.Height)
+		n.windowWidth = msg.Width
+		n.windowHeight = msg.Height
+		return n, globalResizeCmd(msg.Width, msg.Height)
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c":
-			return m, tea.Quit
+			return n, tea.Quit
 		case "l":
-			m.moveRight()
+			n.moveRight()
 		case "h":
-			m.moveLeft()
+			n.moveLeft()
 		}
 	}
-	cursor := m.selector.Cursor()
 	var cmd tea.Cmd
-	m.selector, cmd = m.selector.Update(msg)
-	if cursor != m.selector.Cursor() {
-		m.updateRightTable()
+	cursor := n.selector.Cursor()
+	n.selector, cmd = n.selector.Update(msg)
+	if cursor != n.selector.Cursor() {
+		n.updatePreview()
 	}
 
-	return m, cmd
+	return n, cmd
 }
 
-func (m Navigate) View() string {
-	return lipgloss.JoinHorizontal(0, m.parent.View(), m.selector.View(), m.preview.View())
+func (n Navigate) View() string {
+	return lipgloss.JoinHorizontal(0, n.parent.View(), n.selector.View(), n.preview.View())
 }
