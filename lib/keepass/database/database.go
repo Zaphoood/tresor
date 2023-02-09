e01fb0bef33cfec37462fa92aaabcae83b1558ce
Mathis Weber <mathis.c.weber@web.de>
2023-02-09 15:25:57 +0100

    refactor: read function on header struct

--- lib/keepass/database/database.go
+++ lib/keepass/database/database.go
@@ -9,7 +9,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"sort"
 
@@ -20,81 +19,9 @@ import (
 
 const (
 	VERSION_NUMBER_LEN = 2
-	TLV_TYPE_LEN       = 1
-	TLV_LENGTH_LEN     = 2
 	BLOCK_HASH_LEN     = 32
 	DWORD_LEN          = 4
 )
-
-type headerCode uint8
-
-const (
-	// End of headers
-	EOH headerCode = iota
-	Comment
-	CipherID
-	CompressionFlag
-	MasterSeed
-	TransformSeed
-	TransformRounds
-	EncryptionIV
-	ProtectedStreamKey
-	StreamStartBytes
-	InnerRandomStreamID
-	// Store number of header codes so that we can iterate
-	NUM_HEADER_CODES
-)
-
-// These headers need to be present in order for us to open the database
-var obligatoryHeaders [9]headerCode = [9]headerCode{
-	CipherID,
-	CompressionFlag,
-	MasterSeed,
-	TransformSeed,
-	TransformRounds,
-	EncryptionIV,
-	ProtectedStreamKey,
-	StreamStartBytes,
-	InnerRandomStreamID,
-}
-
-func validHeaderCode(c headerCode) bool {
-	return EOH <= c && c < NUM_HEADER_CODES
-}
-
-var (
-	FILE_SIGNATURE    [4]byte  = [4]byte{0x03, 0xD9, 0xA2, 0x9A}
-	VERSION_SIGNATURE [4]byte  = [4]byte{0x67, 0xFB, 0x4B, 0xB5}
-	AES_CIPHER_ID     [16]byte = [16]byte{0x31, 0xC1, 0xF2, 0xE6, 0xBF, 0x71, 0x43, 0x50, 0xBE, 0x58, 0x05, 0x21, 0x6A, 0xFC, 0x5A, 0xFF}
-)
-
-const (
-	COMPRESSION_None = 0
-	COMPRESSION_GZip = 1
-)
-
-type IRSID int
-
-const (
-	IRS_None = iota
-	IRS_ARC4
-	IRS_Salsa20
-)
-
-func validIRSID(id uint32) bool {
-	return IRS_None <= id && id <= IRS_Salsa20
-}
-
-type databaseHeaders struct {
-	gzipCompression    bool
-	masterSeed         []byte
-	transformSeed      []byte
-	transformRounds    uint64
-	encryptionIV       []byte
-	protectedStreamKey []byte
-	streamStartBytes   []byte
-	irs                IRSID
-}
 
 type block struct {
 	start  int
@@ -102,11 +29,11 @@ type block struct {
 }
 
 type Database struct {
-	path        string
-	verMajor    uint16
-	verMinor    uint16
-	headers     databaseHeaders
-	headers_raw []byte // Store entire headers here; verify hash after decrypting
+	path       string
+	verMajor   uint16
+	verMinor   uint16
+	header     header
+	header_raw []byte // Store entire headers here; verify hash after decrypting
 
 	ciphertext []byte
 	plaintext  []byte
@@ -187,87 +114,23 @@ func (d *Database) Load() error {
 	d.verMinor = binary.LittleEndian.Uint16(bufMinor)
 	d.verMajor = binary.LittleEndian.Uint16(bufMajor)
 
-	// Read headers
-	headerMap := make(map[headerCode][]byte)
-	bufType := make([]byte, TLV_TYPE_LEN)
-	bufLength := make([]byte, TLV_LENGTH_LEN)
-	var (
-		htype  headerCode
-		length uint16
-		value  []byte
-	)
-	for {
-		read, err = f.Read(bufType)
-		if err != nil {
-			return err
-		}
-		if read != len(bufType) {
-			return errors.New("File truncated")
-		}
-		read, err = f.Read(bufLength)
-		if err != nil {
-			return err
-		}
-		if read != len(bufLength) {
-			return errors.New("File truncated")
-		}
-		htype = headerCode(bufType[0])
-		length = binary.LittleEndian.Uint16(bufLength)
-		value = make([]byte, length)
-		read, err = f.Read(value)
-		if htype == EOH {
-			break
-		}
-		if !validHeaderCode(htype) {
-			log.Printf("WARNING: Skipping invalid header code: %d", htype)
-		}
-		headerMap[htype] = value
+	// Read header
+	err = d.header.read(f)
+	if err != nil {
+		return err
 	}
 
-	// Store headers for later hashing
+	// Store raw header content for hashing later
 	headersLength, err := f.Seek(0, 1)
 	if err != nil {
 		return err
 	}
-	d.headers_raw = make([]byte, headersLength)
+	d.header_raw = make([]byte, headersLength)
 	_, err = f.Seek(0, 0)
 	if err != nil {
 		return err
 	}
-	f.Read(d.headers_raw)
-
-	// Parse headers
-	for _, h := range obligatoryHeaders {
-		if _, present := headerMap[h]; !present {
-			return fmt.Errorf("Missing header with code %d", h)
-		}
-	}
-
-	if !bytes.Equal(headerMap[CipherID], AES_CIPHER_ID[:]) {
-		return errors.New("Invalid or unsupported cipher")
-	}
-
-	switch flag := binary.LittleEndian.Uint32(headerMap[CompressionFlag]); flag {
-	case COMPRESSION_None:
-		d.headers.gzipCompression = false
-	case COMPRESSION_GZip:
-		d.headers.gzipCompression = true
-	default:
-		return fmt.Errorf("Unknown compression flag: %d", flag)
-	}
-
-	d.headers.masterSeed = headerMap[MasterSeed]
-	d.headers.transformSeed = headerMap[TransformSeed]
-	d.headers.transformRounds = binary.LittleEndian.Uint64(headerMap[TransformRounds])
-	d.headers.encryptionIV = headerMap[EncryptionIV]
-	d.headers.protectedStreamKey = headerMap[ProtectedStreamKey]
-	d.headers.streamStartBytes = headerMap[StreamStartBytes]
-
-	irsid := binary.LittleEndian.Uint32(headerMap[InnerRandomStreamID])
-	if !validIRSID(irsid) {
-		return fmt.Errorf("Invalid Inner Random Stream ID: %d", irsid)
-	}
-	d.headers.irs = IRSID(irsid)
+	f.Read(d.header_raw)
 
 	// Read remaining file content
 	d.ciphertext, err = ioutil.ReadAll(f)
@@ -288,19 +151,19 @@ func (d *Database) Decrypt(password string) error {
 	compositeKey = sha256.Sum256(compositeKey[:])
 
 	// Generate master key
-	transformOut, err := crypto.AESRounds(compositeKey[:], d.headers.transformSeed, d.headers.transformRounds)
+	transformOut, err := crypto.AESRounds(compositeKey[:], d.header.transformSeed, d.header.transformRounds)
 	if err != nil {
 		return err
 	}
 	transformKey := sha256.Sum256(transformOut)
 
 	h := sha256.New()
-	h.Write(d.headers.masterSeed)
+	h.Write(d.header.masterSeed)
 	h.Write(transformKey[:])
 	masterKey := h.Sum(nil)
 
 	// Decrypt content
-	plaintext, err := crypto.DecryptAES(d.ciphertext, masterKey, d.headers.encryptionIV)
+	plaintext, err := crypto.DecryptAES(d.ciphertext, masterKey, d.header.encryptionIV)
 	if err != nil {
 		return err
 	}
@@ -357,7 +220,7 @@ func (d *Database) Decrypt(password string) error {
 		pos += block.length
 	}
 
-	if d.headers.gzipCompression {
+	if d.header.gzipCompression {
 		out, err := util.Unzip(&d.plaintext)
 		d.plaintext = *out
 		if err != nil {
@@ -369,8 +232,8 @@ func (d *Database) Decrypt(password string) error {
 }
 
 func (d *Database) checkStreamStartBytes(plaintext *[]byte) bool {
-	ok := bytes.Equal(d.headers.streamStartBytes, (*plaintext)[:len(d.headers.streamStartBytes)])
-	*plaintext = (*plaintext)[len(d.headers.streamStartBytes):]
+	ok := bytes.Equal(d.header.streamStartBytes, (*plaintext)[:len(d.header.streamStartBytes)])
+	*plaintext = (*plaintext)[len(d.header.streamStartBytes):]
 	return ok
 }
 
@@ -394,7 +257,7 @@ func (d *Database) VerifyHeaderHash() (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	actualHash := sha256.Sum256(d.headers_raw)
+	actualHash := sha256.Sum256(d.header_raw)
 	// storedHash may be too long, since its length is taken from base64.StdEncoding.DecodedLen
 	// Therefore we only compare the first len(actualHash) bytes
 	return bytes.Equal(actualHash[:], storedHash[:len(actualHash)]), nil
