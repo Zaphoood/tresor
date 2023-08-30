2b66155e1b22aa93b82b02b41f75e7b7084be0bc
Mathis Weber <mathis.c.weber@web.de>
2023-08-30 15:23:34 +0200

    chore: rename variable

--- src/keepass/undo/actions.go
+++ src/keepass/undo/actions.go
@@ -6,19 +6,20 @@ import (
 )
 
 type UpdateEntryAction struct {
-	newEntry    parser.Entry
-	oldEntry    parser.Entry
-	returnValue interface{}
+	newEntry parser.Entry
+	oldEntry parser.Entry
+	// A static value that will be returned on every Do and Undo call
+	afterUpdateReturn interface{}
 }
 
 func (a UpdateEntryAction) Do(p *parser.Document) interface{} {
 	p.UpdateEntry(a.newEntry)
-	return a.returnValue
+	return a.afterUpdateReturn
 }
 
 func (a UpdateEntryAction) Undo(p *parser.Document) interface{} {
 	p.UpdateEntry(a.oldEntry)
-	return a.returnValue
+	return a.afterUpdateReturn
 }
 
 func NewUpdateEntryAction(newEntry, oldEntry parser.Entry, returnValue interface{}) UpdateEntryAction {
